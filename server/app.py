0966522d3181d8e9fafa8d8114a443624f8c3212
Brian <brian.kiprono8@student.moringaschool.com>
2025-06-18 03:28:11 +0300

    conecting bluePrints

--- server/app.py
+++ server/app.py
@@ -3,6 +3,9 @@ from flask_migrate import Migrate
 from .models import db
 from .config import Config
 # from .controllers import restaurant_controller, pizza_controller, restaurant_pizza_controller
+from .controllers.pizza_controller import pizza_bp
+from .controllers.restaurant_controller import restaurant_bp
+from .controllers.restaurant_pizza_controller import restaurant_pizza_bp
 
 
 
@@ -16,6 +19,9 @@ def create_my_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+    app.register_blueprint(pizza_bp)
+    app.register_blueprint(restaurant_bp)
+    app.register_blueprint(restaurant_pizza_bp)
 
     return app
 
